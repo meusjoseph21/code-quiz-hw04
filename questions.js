b4c7adc4358a1b1de95ef6c95d70df5a9b27245a
JosMuse <no-reply>
2020-09-29 09:59:46 -0600

    made question array

--- questions.js
+++ questions.js
@@ -41,7 +41,13 @@ function quizBegin(){
 quizBegin()
 
 
-var set1 = {question: "what is my name?", answer: ["joey","joseph","joe"]}
+var questions = [
+    {title: "what is up?", choices: ['one','two','three','four'], answer: 'one'},
+    {title: "what is down?", choices: ['one','two','three','four'], answer: 'two'},
+    {title: "what is left", choices: ['one','two','three','four'], answer: 'three'},
+    {title: "what is right?", choices: ['one','two','three','four'], answer: 'four'},
+
+]
 
 
 function startQuestions(){
